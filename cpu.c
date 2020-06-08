aea3eca43ae39702fa54eb772d0f432f8683936a
mje349 <mje349@nyu.edu>
2020-06-08 11:07:21 -0700

    initialized makefile and cpu.c

---
+++ cpu.c
@@ -0,0 +1,12 @@
+// Author: Montana Esguerra
+// Date: 6/8/20
+// Description: CPU Scheduler TODO full program description
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "oslabs.h"
+
+int main(int argc, char* argv)
+{
+    return 0;
+}
